df176a3aefacb6cacc856cd39a58cc6b7097cfb8
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 14:41:49 +0100

    Combine tests

--- src/test/kotlin/TransportAppTest.kt
+++ src/test/kotlin/TransportAppTest.kt
@@ -8,16 +8,10 @@ class TransportAppTest {
 
     private val app = TransportApp()
 
-    @Test fun `it takes 5 hours to deliver cargo to a warehouse`() {
+    @Test fun `ships cargo to a warehouse`() {
         app.addWarehouse("B", distance = 5)
         app.ship("B")
         assertThat(app.totalDeliveryTime(), equalTo(5))
-    }
-
-    @Test fun `shipment has to travel the distance to the warehouse`() {
-        app.addWarehouse("B", distance = 3)
-        app.ship("B")
-        assertThat(app.totalDeliveryTime(), equalTo(3))
     }
 
     @Test fun `total delivery time is zero before anything has been shipped`() {
