5bbeb77938878710b8c1c75c81b81eb17c3c3645
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 17:26:56 +0100

    Use Factory in the app to collect and get waiting shipments

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -2,6 +2,8 @@ package tycoon.transport.app
 
 import tycoon.transport.domain.Distance
 import tycoon.transport.domain.DistanceMap
+import tycoon.transport.domain.Factory
+import tycoon.transport.domain.Shipment
 import tycoon.transport.domain.Truck
 import tycoon.transport.domain.WarehouseId
 import tycoon.transport.domain.WarehouseUnknown
@@ -10,8 +12,11 @@ class TransportApp(private val map: DistanceMap) {
     private var distanceDriven = Distance(0)
 
     fun ship(warehouseId: String) {
+        val factory = Factory()
+        factory.collectShipments(listOf(Shipment(WarehouseId(warehouseId))))
+        val shipment = factory.shipmentsWaiting().first()
         val distance = try {
-            map.distanceTo(WarehouseId(warehouseId))
+            map.distanceTo(shipment.destination)
         } catch (e: WarehouseUnknown) {
             throw RuntimeException("Unknown destination")
         }
