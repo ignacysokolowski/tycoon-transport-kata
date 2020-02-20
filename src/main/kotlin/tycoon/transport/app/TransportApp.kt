a7e0308fac64e9c462f14d93a319271e560c3119
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 14:23:57 +0100

    Add warehouses to the application

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -1,13 +1,19 @@
 package tycoon.transport.app
 
 class TransportApp {
+    private val warehouses = mutableListOf<String>()
+
     fun ship(warehouseId: String) {
-        if (warehouseId != "B") {
+        if (!warehouses.contains(warehouseId)) {
             throw RuntimeException("Unknown destination")
         }
     }
 
     fun totalDeliveryTime(): Int {
         return 5
+    }
+
+    fun addWarehouse(warehouseId: String) {
+        this.warehouses.add(warehouseId)
     }
 }
--- src/test/kotlin/TransportAppTest.kt
+++ src/test/kotlin/TransportAppTest.kt
@@ -6,14 +6,16 @@ import tycoon.transport.app.TransportApp
 
 class TransportAppTest {
 
-    @Test fun `it takes 5 hours to deliver cargo to Warehouse B`() {
+    @Test fun `it takes 5 hours to deliver cargo to a warehouse`() {
         val app = TransportApp()
+        app.addWarehouse("B")
         app.ship("B")
         assertThat(app.totalDeliveryTime(), equalTo(5))
     }
 
     @Test fun `can not ship to unknown destinations`() {
         val app = TransportApp()
+        app.addWarehouse("B")
         val exception = assertThrows<RuntimeException> {
             app.ship("X")
         }
