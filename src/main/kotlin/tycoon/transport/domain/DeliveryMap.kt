7ea30adf7d5d799badac76f8ca1a66b7db05d12f
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 15:24:14 +0100

    Work only with the WarehouseId in the delivery map

--- src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
+++ src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
@@ -1,14 +1,14 @@
 package tycoon.transport.domain
 
 class DeliveryMap {
-    private val distances = mutableMapOf<String, Int>()
+    private val distances = mutableMapOf<WarehouseId, Int>()
 
-    fun distanceTo(warehouseId: String): Int {
+    fun distanceTo(warehouseId: WarehouseId): Int {
         return distances[warehouseId]
             ?: throw WarehouseUnknown()
     }
 
     fun addWarehouseWithDistance(distance: Int, warehouseId: WarehouseId) {
-        distances[warehouseId.value] = distance
+        distances[warehouseId] = distance
     }
 }
--- src/test/kotlin/DeliveryMapTest.kt
+++ src/test/kotlin/DeliveryMapTest.kt
@@ -12,12 +12,12 @@ class DeliveryMapTest {
 
     @Test fun `tells the distance to a warehouse`() {
         map.addWarehouseWithDistance(5, WarehouseId("A"))
-        assertThat(map.distanceTo("A"), equalTo(5))
+        assertThat(map.distanceTo(WarehouseId("A")), equalTo(5))
     }
 
     @Test fun `can not tell the distance to unknown warehouses`() {
         assertThrows<WarehouseUnknown> {
-            map.distanceTo("A")
+            map.distanceTo(WarehouseId("A"))
         }
     }
 }
