19aeb08b1a4027b890029621c7ebc3ae515e7905
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 15:16:05 +0100

    Add a map of distances

--- src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
+++ src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
@@ -2,9 +2,10 @@ package tycoon.transport.domain
 
 class DeliveryMap {
     fun distanceTo(warehouseId: String): Int {
+        val distances = mapOf("A" to 5)
         if (warehouseId != "A") {
             throw WarehouseUnknown()
         }
-        return 5
+        return distances[warehouseId]!!
     }
 }
