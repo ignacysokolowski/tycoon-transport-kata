2ae59258bd5430d5cb2678762ac3c89431b61021
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 15:16:48 +0100

    Remove redundant check for warehouse existence on the map

--- src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
+++ src/main/kotlin/tycoon/transport/domain/DeliveryMap.kt
@@ -4,9 +4,7 @@ class DeliveryMap {
     private val distances = mapOf("A" to 5)
 
     fun distanceTo(warehouseId: String): Int {
-        if (warehouseId != "A") {
-            throw WarehouseUnknown()
-        }
-        return distances[warehouseId]!!
+        return distances[warehouseId]
+            ?: throw WarehouseUnknown()
     }
 }
