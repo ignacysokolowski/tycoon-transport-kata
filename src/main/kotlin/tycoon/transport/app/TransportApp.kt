a817322d0f7ec4d9bd622c296b041edc86cb5d64
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 14:42:20 +0100

    Remove redundant 'this'

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -14,6 +14,6 @@ class TransportApp {
     }
 
     fun addWarehouse(warehouseId: String, distance: Int) {
-        this.warehouses[warehouseId] = distance
+        warehouses[warehouseId] = distance
     }
 }
