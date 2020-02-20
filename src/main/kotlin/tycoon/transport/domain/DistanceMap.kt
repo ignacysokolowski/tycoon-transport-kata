564f8bf020b32303c600246fee3ef4ab68e545d0
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 15:45:33 +0100

    Store Distance instances in the distance map

--- src/main/kotlin/tycoon/transport/domain/DistanceMap.kt
+++ src/main/kotlin/tycoon/transport/domain/DistanceMap.kt
@@ -1,14 +1,14 @@
 package tycoon.transport.domain
 
 class DistanceMap {
-    private val distances = mutableMapOf<WarehouseId, Int>()
+    private val distances = mutableMapOf<WarehouseId, Distance>()
 
     fun distanceTo(warehouseId: WarehouseId): Int {
-        return distances[warehouseId]
+        return distances[warehouseId]?.hours
             ?: throw WarehouseUnknown()
     }
 
     fun addWarehouse(warehouseId: WarehouseId, distance: Distance) {
-        distances[warehouseId] = distance.hours
+        distances[warehouseId] = distance
     }
 }
