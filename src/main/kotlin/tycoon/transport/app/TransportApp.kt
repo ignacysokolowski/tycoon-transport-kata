42235e7dbe3c5b9a9f1c710e5b0fd0c3ef1ddc2e
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 15:49:31 +0100

    Store distance driven as a Distance

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -1,19 +1,20 @@
 package tycoon.transport.app
 
+import tycoon.transport.domain.Distance
 import tycoon.transport.domain.DistanceMap
 import tycoon.transport.domain.WarehouseId
 import tycoon.transport.domain.WarehouseUnknown
 
 class TransportApp(private val map: DistanceMap) {
-    private var distanceDriven = 0
+    private var distanceDriven = Distance(0)
 
     fun ship(warehouseId: String) {
         try {
-            distanceDriven = map.distanceTo(WarehouseId(warehouseId)).hours
+            distanceDriven = map.distanceTo(WarehouseId(warehouseId))
         } catch (e: WarehouseUnknown) {
             throw RuntimeException("Unknown destination")
         }
     }
 
-    fun totalDeliveryTime() = distanceDriven
+    fun totalDeliveryTime() = distanceDriven.hours
 }
