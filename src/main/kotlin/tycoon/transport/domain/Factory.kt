46f0c4ca5d24b68e55a4ea31d29bf4565b29a198
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-20 17:23:26 +0100

    Reorder methods

--- src/main/kotlin/tycoon/transport/domain/Factory.kt
+++ src/main/kotlin/tycoon/transport/domain/Factory.kt
@@ -3,9 +3,9 @@ package tycoon.transport.domain
 class Factory {
     private var shipmentsWaiting = emptyList<Shipment>()
 
-    fun shipmentsWaiting() = shipmentsWaiting
-
     fun collectShipments(shipments: List<Shipment>) {
         shipmentsWaiting = shipments
     }
+
+    fun shipmentsWaiting() = shipmentsWaiting
 }
