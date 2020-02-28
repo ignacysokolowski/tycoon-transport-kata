7f9671a2cd814e6f24ab36e14c3176055a7e54ba
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-28 22:55:19 +0100

    Fake telling that trip is not at destination

--- src/main/kotlin/tycoon/transport/domain/Trip.kt
+++ src/main/kotlin/tycoon/transport/domain/Trip.kt
@@ -2,4 +2,7 @@ package tycoon.transport.domain
 
 data class Trip(private val distance: Distance) {
     fun advancedBy(distance: Distance) = Trip(this.distance - distance)
+    fun atDestination(): Boolean {
+        return false
+    }
 }
--- src/test/kotlin/domain/TripTest.kt
+++ src/test/kotlin/domain/TripTest.kt
@@ -8,6 +8,10 @@ import tycoon.transport.domain.Trip
 
 class TripTest {
 
+    @Test fun `is not at destination if has a distance`() {
+        assertThat(Trip(Distance(1)).atDestination(), equalTo(false))
+    }
+
     @Test fun `gets a shorter distance when advanced`() {
         assertThat(Trip(Distance(3)).advancedBy(Distance(1)), equalTo(Trip(Distance(2))))
     }
