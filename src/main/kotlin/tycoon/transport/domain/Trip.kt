4659adbb81b2b2c08db55b5318c05dd85ef03398
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-28 22:55:43 +0100

    Convert Trip.atDestination to expression body

--- src/main/kotlin/tycoon/transport/domain/Trip.kt
+++ src/main/kotlin/tycoon/transport/domain/Trip.kt
@@ -2,7 +2,5 @@ package tycoon.transport.domain
 
 data class Trip(private val distance: Distance) {
     fun advancedBy(distance: Distance) = Trip(this.distance - distance)
-    fun atDestination(): Boolean {
-        return false
-    }
+    fun atDestination() = false
 }
