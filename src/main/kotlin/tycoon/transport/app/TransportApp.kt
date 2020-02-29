15ea9e22198fcc20ecba8fd6dc645fea709b26d8
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 12:21:12 +0100

    Pass ID to each shipment in the application

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -4,6 +4,7 @@ import tycoon.transport.domain.Distance
 import tycoon.transport.domain.DistanceMap
 import tycoon.transport.domain.Factory
 import tycoon.transport.domain.Shipment
+import tycoon.transport.domain.ShipmentIds
 import tycoon.transport.domain.Trip
 import tycoon.transport.domain.Truck
 import tycoon.transport.domain.WarehouseId
@@ -12,6 +13,7 @@ import tycoon.transport.domain.WarehouseUnknown
 class TransportApp(private val map: DistanceMap) {
     private val factory = Factory()
     private val truck = Truck.on(Trip(Distance(0)))
+    private val shipmentIds = ShipmentIds()
     private var distanceDriven = Distance(0)
 
     fun ship(warehouseIds: List<String>) {
@@ -27,7 +29,7 @@ class TransportApp(private val map: DistanceMap) {
     }
 
     private fun shipmentsFrom(warehouseIds: List<String>) =
-        warehouseIds.map { Shipment(WarehouseId(it)) }
+        warehouseIds.map { Shipment(WarehouseId(it), shipmentIds.next()) }
 
     private fun ship(shipment: Shipment) {
         val distance = map.distanceTo(shipment.destination)
