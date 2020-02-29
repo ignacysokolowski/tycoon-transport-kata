26b3b3ee812a1376b5b9cfb5ed7ca9f1081eb0eb
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 21:22:31 +0100

    Make journey's constructor private

--- src/main/kotlin/tycoon/transport/domain/Trip.kt
+++ src/main/kotlin/tycoon/transport/domain/Trip.kt
@@ -1,6 +1,9 @@
 package tycoon.transport.domain
 
-class Trip(private val destination: LocationId, private val distance: Distance) {
+class Trip private constructor(
+    private val destination: LocationId,
+    private val distance: Distance
+) {
     companion object {
         fun between(origin: LocationId, destination: LocationId, distance: Distance): Trip {
             return Trip(destination, distance)
