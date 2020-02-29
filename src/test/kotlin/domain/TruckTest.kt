6bb81dc38d31dd591fa213bebac7d64aa3198fdf
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 15:05:00 +0100

    Pass trip destination in all truck tests

--- src/test/kotlin/domain/TruckTest.kt
+++ src/test/kotlin/domain/TruckTest.kt
@@ -5,6 +5,7 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import tycoon.transport.domain.Distance
+import tycoon.transport.domain.LocationId
 import tycoon.transport.domain.Trip
 import tycoon.transport.domain.Truck
 import tycoon.transport.domain.TruckAtDestination
@@ -12,43 +13,43 @@ import tycoon.transport.domain.TruckAtDestination
 class TruckTest {
 
     @Test fun `has not driven yet`() {
-        val truck = Truck.on(Trip(Distance(0)))
+        val truck = Truck.on(Trip(Distance(0), LocationId("A")))
         assertThat(truck.distanceDriven(), equalTo(Distance(0)))
     }
 
     @Test fun `not at the destination until has driven the whole trip distance`() {
-        val truck = Truck.on(Trip(Distance(1)))
+        val truck = Truck.on(Trip(Distance(1), LocationId("A")))
         assertThat(truck.atDestination(), equalTo(false))
     }
 
     @Test fun `at the destination once has driven the whole trip distance`() {
-        val truck = Truck.on(Trip(Distance(0)))
+        val truck = Truck.on(Trip(Distance(0), LocationId("A")))
         assertThat(truck.atDestination(), equalTo(true))
     }
 
     @Test fun `drives the trip distance`() {
-        val truck = Truck.on(Trip(Distance(3)))
+        val truck = Truck.on(Trip(Distance(3), LocationId("A")))
         truck.drive(Distance(2))
         truck.drive(Distance(1))
         assertThat(truck.atDestination(), equalTo(true))
     }
 
     @Test fun `can not drive if already at destination`() {
-        val truck = Truck.on(Trip(Distance(0)))
+        val truck = Truck.on(Trip(Distance(0), LocationId("A")))
         assertThrows<TruckAtDestination> {
             truck.drive(Distance(5))
         }
     }
 
     @Test fun `can start a new trip`() {
-        val truck = Truck.on(Trip(Distance(0)))
-        truck.startTrip(Trip(Distance(1)))
+        val truck = Truck.on(Trip(Distance(0), LocationId("A")))
+        truck.startTrip(Trip(Distance(1), LocationId("B")))
         truck.drive(Distance(1))
         assertThat(truck.atDestination(), equalTo(true))
     }
 
     @Test fun `records the distance it has driven`() {
-        val truck = Truck.on(Trip(Distance(10)))
+        val truck = Truck.on(Trip(Distance(10), LocationId("A")))
         truck.drive(Distance(5))
         truck.drive(Distance(3))
         assertThat(truck.distanceDriven(), equalTo(Distance(8)))
