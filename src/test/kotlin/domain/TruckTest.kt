a2fb4eee0b5948594910ddc4eb9d1d055e20a412
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 22:00:43 +0100

    Create trucks on trips in all truck tests

--- src/test/kotlin/domain/TruckTest.kt
+++ src/test/kotlin/domain/TruckTest.kt
@@ -21,69 +21,69 @@ class TruckTest {
     }
 
     @Test fun `not at the destination until has driven the whole journey distance`() {
-        Truck.on(Journey.to(LocationId("A"), Distance(1)), truckListener)
+        Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(1)), truckListener)
         assertThat(truckListener.arrivals, equalTo(emptyList<TruckArrival>()))
     }
 
     @Test fun `at the destination once has driven the whole journey distance`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(0)), truckListener)
-        assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("A")))))
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(0)), truckListener)
+        assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("B")))))
     }
 
     @Test fun `drives the journey distance`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(3)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(3)), truckListener)
         truck.drive(Distance(2))
         truck.drive(Distance(1))
-        assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("A")))))
+        assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("B")))))
     }
 
     @Test fun `only notifies about actual arrivals`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(3)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(3)), truckListener)
         truck.drive(Distance(2))
         assertThat(truckListener.arrivals, equalTo(emptyList<TruckArrival>()))
     }
 
     @Test fun `does not drive if already at destination`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(0)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(0)), truckListener)
         truck.drive(Distance(1))
         assertThat(truck.distanceDriven(), equalTo(Distance(0)))
     }
 
     @Test fun `can start a new journey`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(0)), truckListener)
-        truck.startJourney(Journey.to(LocationId("B"), Distance(1)))
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(0)), truckListener)
+        truck.startJourney(Journey.to(LocationId("A"), Distance(1)))
         truck.drive(Distance(1))
         assertThat(
             truckListener.arrivals,
             equalTo(listOf(
-                TruckArrival(truck, LocationId("A")),
-                TruckArrival(truck, LocationId("B"))
+                TruckArrival(truck, LocationId("B")),
+                TruckArrival(truck, LocationId("A"))
             ))
         )
     }
 
     @Test fun `records the distance it has driven`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(10)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(10)), truckListener)
         truck.drive(Distance(5))
         truck.drive(Distance(3))
         assertThat(truck.distanceDriven(), equalTo(Distance(8)))
     }
 
     @Test fun `picks up shipments`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(10)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(10)), truckListener)
         truck.pickUp(ShipmentId("1"))
         assertThat(truck.dropOff(), equalTo(ShipmentId("1")))
     }
 
     @Test fun `can not drop off shipments if did not pick up`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(10)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(10)), truckListener)
         assertThrows<NoShipmentCarried> {
             truck.dropOff()
         }
     }
 
     @Test fun `has to pick up another shipment after dropping off`() {
-        val truck = Truck.on(Journey.to(LocationId("A"), Distance(10)), truckListener)
+        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(10)), truckListener)
         truck.pickUp(ShipmentId("1"))
         truck.dropOff()
         assertThrows<NoShipmentCarried> {
