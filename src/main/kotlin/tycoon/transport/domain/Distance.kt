38ff946a6fc235155bf01413c730371ca2225088
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 19:59:38 +0100

    Make sure there is no negative distance

--- src/main/kotlin/tycoon/transport/domain/Distance.kt
+++ src/main/kotlin/tycoon/transport/domain/Distance.kt
@@ -1,6 +1,12 @@
 package tycoon.transport.domain
 
 data class Distance(val hours: Int) {
+    init {
+        if (hours < 0) {
+            throw IllegalArgumentException()
+        }
+    }
+
     operator fun plus(other: Distance) = Distance(hours + other.hours)
     operator fun minus(other: Distance) = Distance(hours - other.hours)
 }
--- src/test/kotlin/domain/DistanceTest.kt
+++ src/test/kotlin/domain/DistanceTest.kt
@@ -3,9 +3,16 @@ package domain
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import tycoon.transport.domain.Distance
 
 class DistanceTest {
+
+    @Test fun `can not be negative`() {
+        assertThrows<IllegalArgumentException> {
+            Distance(-1)
+        }
+    }
 
     @Test fun `can be added to another distance`() {
         assertThat(Distance(2) + Distance(3), equalTo(Distance(5)))
