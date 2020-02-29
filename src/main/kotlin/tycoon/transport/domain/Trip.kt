73e4dbf1b868f21df4d42820122991c878e9ba94
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 21:21:56 +0100

    Get journey distance from the trip

--- src/main/kotlin/tycoon/transport/domain/Trip.kt
+++ src/main/kotlin/tycoon/transport/domain/Trip.kt
@@ -1,13 +1,13 @@
 package tycoon.transport.domain
 
-class Trip(private val destination: LocationId) {
+class Trip(private val destination: LocationId, private val distance: Distance) {
     companion object {
         fun between(origin: LocationId, destination: LocationId, distance: Distance): Trip {
-            return Trip(destination)
+            return Trip(destination, distance)
         }
     }
 
     fun journey(): Journey {
-        return Journey.to(destination, Distance(3))
+        return Journey.to(destination, distance)
     }
 }
