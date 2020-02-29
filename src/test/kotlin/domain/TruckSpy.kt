d71ff62ce80342073d036025aba0049b98f8aed6
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 16:31:00 +0100

    Add a TruckArrival object

--- src/test/kotlin/domain/TruckSpy.kt
+++ src/test/kotlin/domain/TruckSpy.kt
@@ -3,14 +3,16 @@ package domain
 import tycoon.transport.domain.LocationId
 import tycoon.transport.domain.Truck
 
+data class TruckArrival(val truck: Truck, val destination: LocationId)
+
 class TruckSpy {
-    private val arrivals = mutableListOf<Pair<Truck, LocationId>>()
+    private val arrivals = mutableListOf<TruckArrival>()
 
     fun arrivalsOf(truck: Truck): List<LocationId> {
-        return arrivals.filter { it.first == truck }.map { it.second }
+        return arrivals.filter { it.truck == truck }.map { it.destination }
     }
 
     fun truckArrived(truck: Truck, locationId: LocationId) {
-        arrivals.add(Pair(truck, locationId))
+        arrivals.add(TruckArrival(truck, locationId))
     }
 }
