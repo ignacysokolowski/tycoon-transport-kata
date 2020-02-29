5241cbe1fca44ba766e9f76206aa459c7b2fdefc
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 22:09:59 +0100

    Inline the truck creation method accepting a journey

--- src/main/kotlin/tycoon/transport/domain/Truck.kt
+++ src/main/kotlin/tycoon/transport/domain/Truck.kt
@@ -6,8 +6,7 @@ class Truck private constructor(
 ) {
 
     companion object {
-        fun on(journey: Journey, listener: TruckListener) = Truck(journey, listener)
-        fun on(trip: Trip, listener: TruckListener) = on(trip.journey(), listener)
+        fun on(trip: Trip, listener: TruckListener) = Truck(trip.journey(), listener)
     }
 
     private var distanceDriven = Distance(0)
