aa79e726e759170db194aa68156b0df7f3459248
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-29 12:14:50 +0100

    Remove redundant shipment IDs test

--- src/test/kotlin/domain/ShipmentIdsTest.kt
+++ src/test/kotlin/domain/ShipmentIdsTest.kt
@@ -8,10 +8,6 @@ import tycoon.transport.domain.ShipmentIds
 class ShipmentIdsTest {
     private val ids = ShipmentIds()
 
-    @Test fun `first shipment ID is 1`() {
-        assertThat(ids.next(), equalTo("1"))
-    }
-
     @Test fun `generates a sequence of numbered shipment IDs`() {
         assertThat((1..3).map { ids.next() }, equalTo(listOf("1", "2", "3")))
     }
