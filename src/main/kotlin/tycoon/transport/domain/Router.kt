0b9092889e8b09257d14498520ee6a0ddd8fe2d8
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-03-01 11:06:31 +0100

    Make router create trips from origin to destination

--- src/main/kotlin/tycoon/transport/domain/Router.kt
+++ src/main/kotlin/tycoon/transport/domain/Router.kt
@@ -2,4 +2,8 @@ package tycoon.transport.domain
 
 class Router(private val origin: LocationId) {
     fun inPlaceTripAtOrigin() = Trip.inPlace(origin)
+
+    fun tripTo(locationId: LocationId): Trip {
+        return Trip.between(origin, locationId, Distance(3))
+    }
 }
--- src/test/kotlin/domain/RouterTest.kt
+++ src/test/kotlin/domain/RouterTest.kt
@@ -3,6 +3,7 @@ package domain
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import tycoon.transport.domain.Distance
 import tycoon.transport.domain.LocationId
 import tycoon.transport.domain.Router
 import tycoon.transport.domain.Trip
@@ -14,5 +15,12 @@ class RouterTest {
 
     @Test fun `creates an in-place trip at the origin`() {
         assertThat(router.inPlaceTripAtOrigin(), equalTo(Trip.inPlace(originLocationId)))
+    }
+
+    @Test fun `creates trips from the origin to a destination`() {
+        assertThat(
+            router.tripTo(LocationId("D")),
+            equalTo(Trip.between(originLocationId, LocationId("D"), Distance(3)))
+        )
     }
 }
