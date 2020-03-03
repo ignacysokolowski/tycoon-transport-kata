7dac4d28b465b30c29fb2b82dbe6303717386200
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-03-03 20:24:43 +0100

    Place the truck on an in-place trip in the test for not driving in place

--- src/test/kotlin/domain/TruckTest.kt
+++ src/test/kotlin/domain/TruckTest.kt
@@ -38,7 +38,7 @@ class TruckTest {
     }
 
     @Test fun `does not drive if already at destination`() {
-        val truck = Truck.on(Trip.between(LocationId("A"), LocationId("B"), Distance(0)), truckListener)
+        val truck = Truck.on(Trip.inPlace(LocationId("B")), truckListener)
         assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("B")))))
         truck.drive(Distance(1))
         assertThat(truckListener.arrivals, equalTo(listOf(TruckArrival(truck, LocationId("B")))))
