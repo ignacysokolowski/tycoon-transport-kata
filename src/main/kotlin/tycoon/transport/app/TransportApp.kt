18ed8a0be688b195ddfc8710d7f12daa6c1345af
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-03-22 15:07:57 +0100

    Use the StopWatch in the app

--- src/main/kotlin/tycoon/transport/app/TransportApp.kt
+++ src/main/kotlin/tycoon/transport/app/TransportApp.kt
@@ -7,17 +7,20 @@ import tycoon.transport.domain.Factory
 import tycoon.transport.domain.LocationId
 import tycoon.transport.domain.LocationUnknown
 import tycoon.transport.domain.MapRouter
+import tycoon.transport.domain.StopWatch
+import tycoon.transport.domain.TimeListener
 import tycoon.transport.domain.TransportArrivalNotifier
 import tycoon.transport.domain.TransportMap
 import tycoon.transport.domain.Truck
 import tycoon.transport.domain.Warehouse
 
-class TransportApp {
+class TransportApp : TimeListener {
     private val cargoIds = CargoIds()
     private val factory = Factory()
     private val map = TransportMap(factory)
     private val truckRouter = MapRouter(factory.locationId, map)
     private val transportArrivalNotifier = TransportArrivalNotifier(map)
+    private val stopWatch = StopWatch(this)
     private var numberOfTrucks = 0
     private var trucks = mutableListOf<Truck>()
     private var totalDeliveryTime = 0
@@ -51,13 +54,15 @@ class TransportApp {
     private fun ship(cargoes: List<Cargo>) {
         factory.produce(cargoes)
         parkTrucksAtTheFactory()
-        while (!factory.hasAllCargoesDelivered()) {
-            trucks.forEach { it.drive(Distance(1)) }
-            totalDeliveryTime += 1
-        }
+        stopWatch.countUntil { factory.hasAllCargoesDelivered() }
     }
 
     private fun parkTrucksAtTheFactory() = repeat(numberOfTrucks) { trucks.add(newTruck()) }
 
     private fun newTruck() = Truck.parked(truckRouter, transportArrivalNotifier)
+
+    override fun tick() {
+        trucks.forEach { it.drive(Distance(1)) }
+        totalDeliveryTime += 1
+    }
 }
