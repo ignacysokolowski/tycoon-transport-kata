ea006d26fc7dfa824ea510cdea99a9adb05b898d
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-03-22 14:45:11 +0100

    Extract a field for test stopwatch

--- src/test/kotlin/domain/StopWatchTest.kt
+++ src/test/kotlin/domain/StopWatchTest.kt
@@ -7,8 +7,9 @@ import tycoon.transport.domain.StopWatch
 
 class StopWatchTest {
 
+    private val stopWatch = StopWatch()
+
     @Test fun `no time elapsed before starting`() {
-        val stopWatch = StopWatch()
         assertThat(stopWatch.timeElapsed, equalTo(0))
     }
 }
