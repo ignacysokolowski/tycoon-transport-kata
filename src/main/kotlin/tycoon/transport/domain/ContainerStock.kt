0bed12f4f5dda060d4cd51ca3eb0074ad61dc97c
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-03-22 21:30:16 +0100

    Remove tracking delivery from ContainerStock
    It's not used anymore since the functionality was moved from the Factory
    to the DeliveryTracker.

--- src/main/kotlin/tycoon/transport/domain/ContainerStock.kt
+++ src/main/kotlin/tycoon/transport/domain/ContainerStock.kt
@@ -4,7 +4,6 @@ import java.util.ArrayDeque
 
 class ContainerStock {
     private var waiting = ArrayDeque<Cargo>()
-    private val pickedUp = mutableListOf<Cargo>()
 
     fun put(cargoes: List<Cargo>) {
         waiting = ArrayDeque(cargoes)
@@ -16,15 +15,6 @@ class ContainerStock {
         } catch (e: NoSuchElementException) {
             throw AllCargoPickedUp()
         }
-        pickedUp.add(cargo)
         return cargo
     }
-
-    fun markDelivered(cargoId: CargoId) {
-        if (!pickedUp.removeIf { it.id == cargoId }) {
-            throw CargoNotPickedUp()
-        }
-    }
-
-    fun allDelivered() = pickedUp.isEmpty() and waiting.isEmpty()
 }
