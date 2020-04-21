c7efaa9f8aa0c32e09443f136a40fcc5d9a7a3cc
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-04-21 09:02:04 +0200

    Add a focused test for the container stack with all cargo picked up

--- src/test/kotlin/domain/delivery/ContainerStackTest.kt
+++ src/test/kotlin/domain/delivery/ContainerStackTest.kt
@@ -3,9 +3,11 @@ package domain.delivery
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import tycoon.transport.domain.Location
 import tycoon.transport.domain.cargo.Cargo
 import tycoon.transport.domain.cargo.CargoId
+import tycoon.transport.domain.delivery.AllCargoPickedUp
 import tycoon.transport.domain.delivery.ContainerStack
 
 class ContainerStackTest {
@@ -19,5 +21,12 @@ class ContainerStackTest {
         stack.put(cargoes)
         assertThat(stack.pickUpNext(), equalTo(cargoes[0]))
         assertThat(stack.pickUpNext(), equalTo(cargoes[1]))
+    }
+
+    @Test fun `does not allow to pick up anymore after all cargo have been picked up`() {
+        val stack = ContainerStack()
+        stack.put(listOf(Cargo(CargoId("1"), Location("A"))))
+        stack.pickUpNext()
+        assertThrows<AllCargoPickedUp> { stack.pickUpNext() }
     }
 }
