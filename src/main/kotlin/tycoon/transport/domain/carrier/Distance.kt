8e46f998653e52113c779f3c31564f95e7c28e70
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-04-29 09:36:05 +0200

    Convert Distance hours validation to a 'require' function

--- src/main/kotlin/tycoon/transport/domain/carrier/Distance.kt
+++ src/main/kotlin/tycoon/transport/domain/carrier/Distance.kt
@@ -2,9 +2,7 @@ package tycoon.transport.domain.carrier
 
 data class Distance(private val hours: Int) {
     init {
-        if (hours < 0) {
-            throw IllegalArgumentException("Distance can not be negative")
-        }
+        require(hours >= 0) { "Distance can not be negative" }
     }
 
     operator fun minus(other: Distance) = Distance(hours - other.hours)
