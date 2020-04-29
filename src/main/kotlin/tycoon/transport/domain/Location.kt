1ac004c0e2e702f2d74220d14b0ae1eec99eed2b
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-04-29 09:35:24 +0200

    Add an exception message for empty location code

--- src/main/kotlin/tycoon/transport/domain/Location.kt
+++ src/main/kotlin/tycoon/transport/domain/Location.kt
@@ -2,6 +2,6 @@ package tycoon.transport.domain
 
 data class Location(private val code: String) {
     init {
-        require(code.isNotEmpty())
+        require(code.isNotEmpty()) { "Location code can not be empty" }
     }
 }
--- src/test/kotlin/domain/LocationTest.kt
+++ src/test/kotlin/domain/LocationTest.kt
@@ -1,5 +1,7 @@
 package domain
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import tycoon.transport.domain.Location
@@ -7,8 +9,9 @@ import tycoon.transport.domain.Location
 class LocationTest {
 
     @Test fun `code can not be empty`() {
-        assertThrows<IllegalArgumentException> {
+        val error = assertThrows<IllegalArgumentException> {
             Location("")
         }
+        assertThat(error.message, equalTo("Location code can not be empty"))
     }
 }
